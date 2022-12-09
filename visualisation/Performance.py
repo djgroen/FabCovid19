3e9b074e7baf1b028b69d43cc903a65886ebff6f
Arindam Saha <arindamsaha1507@gmail.com>
2022-12-09 10:58:06 +0000

    Significant improvements to the quality of plots

--- visualisation/Performance.py
+++ visualisation/Performance.py
@@ -1,6 +1,7 @@
 import sys
 import os
-import plotly.express as px
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 from plugins.FabCovid19.FabCovid19 import *
 from .facs_postprocess_utils import *
@@ -8,68 +9,85 @@ from .facs_postprocess_utils import *
 
 @task
 @load_plugin_env_vars("FabCovid19")
-def facs_performance(region, machine, cores):
+def facs_performance(regions, machine, cores, mode):
 
     cores = cores.split(';')
+    regions = regions.split(';')
 
-    if ';' in region:
-        sys.exit('Only one region allowed in performance analysis. But {} were given'.format(len(region.split(';'))))
     if ';' in machine:
         sys.exit('Only one machine allowed in performance analysis. But {} were given'.format(len(machine.split(';'))))
 
-    Cores = []
-    Times = []
+    if mode == 'speedup':
+        flag = True
+    elif mode == 'time':
+        flag = False
+    else:
+        sys.exit('mode must be either speedup or time...')
 
-    for cc in cores:
+    fig, ax = plt.subplots(figsize=(10,10))
 
-        file_list = []
+    for region in regions:
 
-        dir = "{}/{}_{}_{}/RUNS".format(env.local_results, region, machine, cc)
+        Cores = []
+        Times = []
 
-        for rr in os.listdir(dir):
-            filedir = "{}/{}".format(dir, rr)
 
-            file = list(x for x in os.listdir(filedir) if x.split('.')[-1]=='output')
-            for f in file:
-                f = "{}/{}".format(filedir, f)
-                file_list.append(f)
+        for cc in cores:
 
-        times = []
+            file_list = []
 
-        for ff in file_list:
+            dir = "{}/{}_{}_{}/RUNS".format(env.local_results, region, machine, cc)
 
-            with open(ff, 'r') as f:
-                lines = f.read().splitlines()
-                if len(lines) == 0:
-                   sys.exit('File {} not found'.format(ff))
-                last_line = lines[-1]
-            
-            times.append(last_line.split(' ')[-2])
+            for rr in os.listdir(dir):
+                filedir = "{}/{}".format(dir, rr)
 
-        for ii in range(len(times)):
-            tt = times[ii]
-            try:
-                float(tt)
-            except ValueError:
-                sys.exit('Siimulation in file {} has not completed'.format(file_list[ii]))
-            Cores.append(int(cc))
-            Times.append(float(tt))
+                file = list(x for x in os.listdir(filedir) if x.split('.')[-1]=='output')
+                for f in file:
+                    f = "{}/{}".format(filedir, f)
+                    file_list.append(f)
+
+            times = []
+
+            for ff in file_list:
+
+                with open(ff, 'r') as f:
+                    lines = f.read().splitlines()
+                    if len(lines) == 0:
+                        sys.exit('File {} not found'.format(ff))
+                    last_line = lines[-1]
+                
+                times.append(last_line.split(' ')[-2])
+
+            for ii in range(len(times)):
+                tt = times[ii]
+                try:
+                    float(tt)
+                except ValueError:
+                    sys.exit('Siimulation in file {} has not completed'.format(file_list[ii]))
+                Cores.append(int(cc))
+                Times.append(float(tt))
 
         Speedup = [Times[0]/x for x in Times]
 
-    fig = px.scatter(x=Cores, y=Speedup,
-                    labels={'x': 'Cores', 'y': 'Speedup'},
-                    log_x=True,
-                    log_y=True,
-                    )
+        mean_time = []
+        mean_speedup = []
 
-    fig.update_layout({
-            'plot_bgcolor': 'rgba(255, 255, 255, 255)',
-            'paper_bgcolor': 'rgba(255, 255, 255, 255)',
-            })
+        for cc in cores:
 
-    fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-    fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
+            tt = [Times[i] for i in range(len(Times)) if int(cc) == Cores[i]]
+            ss = [Speedup[i] for i in range(len(Speedup)) if int(cc) == Cores[i]]
 
-    fig.show()
+            mean_time.append(sum(tt)/len(tt))
+            mean_speedup.append(sum(ss)/len(tt))
 
+        cores = [int(x) for x in cores]
+
+        if flag:
+            ax.loglog(cores, mean_speedup, '.-')
+            ax.loglog(Cores, Speedup, '.')
+        else:
+            ax.loglog(cores, mean_time, '.-')
+            ax.loglog(Cores, Times, '.')
+            
+
+    plt.show()
